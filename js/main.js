310318c60c1a1b12fd0ce3e96112aa29e3c48ac1
Gaya <sehja@yandex.ru>
2021-02-13 22:46:38 +0300

    fight with swiper feedback section

--- js/main.js
+++ js/main.js
@@ -44,4 +44,33 @@ var swiper = new Swiper('.swiper-container-2', {
 
 
 
+
+
+var swiper = new Swiper('.swiper-container-3', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: '1',
+  loop: true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-3',
+    clickable: 'true',
+    bulletClass: 'swiper-pagination-bullet-3',
+    bulletActiveClass: 'swiper-pagination-bullet-active-3',
+
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-3',
+    prevEl: '.swiper-button-prev-3',
+  }
+});
+
+
+
+
+
 /////////// SWIPER-SLIDER FINISH ///////////
